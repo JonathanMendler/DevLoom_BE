c07f57e4be9050ac270c0ecd5f5fdc0b0acb2182
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-04 11:10:27 -0600

    Add seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Clear existing data
+Post.destroy_all
+
+# Seed data
+Post.create(date: Date.today, title: "Demo Post 1", body: "Lorem ipsum dolor sit amet")
+Post.create(date: Date.today - 1.day, title: "Demo Post 2", body: "Lorem ipsum dolor sit amet")
