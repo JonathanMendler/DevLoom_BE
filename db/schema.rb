a4dd8afbf35a6eeecb4bb7318ebd1ca83ca8c782
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-25 23:00:29 -0500

    Revert "Change from sqlite3 to postgres"
    This reverts commit 01d1f91e862322493cb4db364b723b0597254907.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_03_06_161439) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "posts", force: :cascade do |t|
     t.date "date"
     t.string "title"
